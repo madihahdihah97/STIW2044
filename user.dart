64edc1a8c83e899822898e18d60ec8a69e0b0855
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-10 21:33:02 +0800

    Create user.dart

---
+++ user.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+
+class User{
+
+final String uid;
+
+User({this.uid});
+
+}
+
+
+class UserData {
+
+  final String uid;
+  final String item;
+  final String color;
+  final String wherefound;
+  final String datefound;
+
+  final String name;
+  final String matricno;
+   
+  final String hintText;
+  final String wheref;
+  final String datef;
+ final  String fby;
+  final String deliverymethod;
+  final String validator;
+  final String onSaved;
+  final String isPassword;
+  final String isEmail;
+  final String typeItem;
+  final String colorOrmodel;
+ 
+ 
+  final String foundby;
+  final String typeselected;
+ 
+  final String number;
+ 
+  final String datelost;
+  final String wherelost;
+  final String colorlost;
+  final String email;
+  final String password;
+  final String icno;
+  final String gender;
+  final String inasis;
+  final String phone;
+  final String faculty;
+  final String fname;
+  
+
+
+
+
+
+
+  UserData({ this.uid, this.item, this.color, this.wherefound, this.datefound,
+  
+
+  this.datelost, this.wherelost,this.colorlost,this.email,this.password,this.icno,this.gender,this.phone,this.faculty,this.fname,this.matricno,
+  this.foundby,this.typeselected,this.name,this.number,this.hintText,this.wheref,this.datef,this.fby,this.deliverymethod,this.validator,
+  this.onSaved,this.isEmail,this.isPassword,this.typeItem,this.colorOrmodel,this.inasis,
+  
+  
+  
+  
+   });
+  
+}
